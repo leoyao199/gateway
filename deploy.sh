45d7c2750a0609112fb2d5d1256d99442e1af517
leo.yao <leo.yau@tecky.io>
2023-07-26 17:30:39 +0800

    cicd

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Go to the app's directory
+cd /my-app
+# Pull the latest changes from the repository
+git pull origin main
+
+# Install Node.js dependencies
+npm install
+
+# Build the Next.js app
+npm run build
+
+# Restart the Node.js server (replace 'your_nextjs_app_port' with the actual port)
+pm2 stop your_nextjs_app_port
+pm2 start npm --name "your_nextjs_app_name" -- run start --port=your_nextjs_app_port
